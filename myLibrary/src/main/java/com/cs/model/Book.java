576e9522948b38a814392547f7bf05e429b4070b
Oleksandr Kosyuta <Kocyta@MacBook-Pro-Oleksandr.local>
2017-12-20 20:18:43 +0200

    add Book model

---
+++ myLibrary/src/main/java/com/cs/model/Book.java
@@ -0,0 +1,20 @@
+package com.cs.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+    private long id;
+    private String name;
+    private BookType type;
+
+    public Book(long id, String name, BookType type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+}
