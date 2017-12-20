eed4d9884412f5ec33355d9039c5f7b1a971e5bb
Oleksandr Kosyuta <Kocyta@MacBook-Pro-Oleksandr.local>
2017-12-20 21:13:33 +0200

    add Dao layer

---
+++ myLibrary/src/main/java/com/cs/mapper/BookMapper.java
@@ -0,0 +1,20 @@
+package com.cs.mapper;
+
+import com.cs.model.Book;
+import com.cs.model.BookType;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookMapper implements RowMapper<Book> {
+    @Override
+    public Book mapRow(ResultSet resultSet, int i) throws SQLException {
+        Book book = new Book();
+        book.setId(resultSet.getLong("id"));
+        book.setName(resultSet.getString("name"));
+        BookType type = BookType.valueOf(resultSet.getString("type"));
+        book.setType(type);
+        return book;
+    }
+}
